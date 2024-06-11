83d330c32f89c5098407de6c0fbdd586402b5c31
AlmirPaulo <ap.freelas@gmail.comclear>
2024-06-11 19:59:05 -0300

    first things first

--- bot.sh
+++ bot.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 ########################################################
-#Software:
-#Description:
+#Software: Crypto Spider
+#Description: A bot that get data about cryptocurrency rates and alert you when it reaches the price you want. 
 #Developer: Almir Paulo <freelancer@almirpaulo.com>
 #Repo: 
 #Version: Alpha
@@ -10,14 +10,44 @@
 ########################################################
 
 #Check dependencies
+#curl
+
 
 #Variables
-URL="https://api.coinlore.net/api/tickers/?start=0&limit=100" #Precisa dar escape nas barras?
-TARGET_PRICE=`cat target_price.txt` 
-#echo $TARGET_PRICE
+URL="https://api.coinlore.net/api/tickers/?start=0&limit=100" 
+TARGET_PRICE=$1
+TRADER_CASE=$2
+EMAIL=$3
+VERSION="Alpha"
+HELP=" \t  --bot to get data about cryptocurrency rates and alert you when it reaches the price you want.\n
+    \t\t -h     \tshow this help message \n
+    \t\t -v     \tshow actual software version \n 
+    \t\t -g     \tshow the full guide" 
 
 
-#Run
 
 
+#Settings
+if [ "$1" == "-h" ]; then
+    echo -e $HELP
+elif [ "$1" == "-v" ]; then
+    echo $VERSION
+elif [ "$1" == "-g" ]; then
+    cat guide.txt #to do
+    fi
 
+#Data handling function
+
+#Email sending function
+
+
+#Loop
+#while [ 1 -gt 0 ]
+#do 
+   # curl --request GET --url $URL --header "accept: application/json"
+   #Parse the data
+   #Put it in a csv file
+   #check if the price reach the point
+        #alert by email, if it does
+   # sleep 10 #Just for dev proposals
+#done
