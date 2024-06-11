fc6264650ada012102cffa4fda608790800e56b6
AlmirPaulo <ap.freelas@gmail.comclear>
2024-06-11 18:21:27 -0300

    first commit

---
+++ bot.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+########################################################
+#Software:
+#Description:
+#Developer: Almir Paulo <freelancer@almirpaulo.com>
+#Repo: 
+#Version: Alpha
+#License: MIT
+########################################################
+
+#Check dependencies
+
+#Variables
+URL="https://api.coinlore.net/api/tickers/?start=0&limit=100" #Precisa dar escape nas barras?
+TARGET_PRICE=`cat target_price.txt` 
+#echo $TARGET_PRICE
+
+
+#Run
+
+
+
